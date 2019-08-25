d25a229d9bbe875b5a73345507633ac73e664baa
Jane Wysoki <jane.wysoki@gmail.com>
2019-08-25 01:32:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,6 +6,8 @@ class Anagram
   end
   
   def match(word_array)
-    word_array.select do |element|
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
   end
 end
